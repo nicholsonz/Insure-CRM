91c640dc97df23fa23d75bd85dd9be9316bc221f
Zachary Nicholson <ztnicholson@gmail.com>
2024-04-03 19:17:43 -0500

    modified:   .gitignore 	modified:   dbconfig.php 	modified:   register-old.php 	modified:   register.php

--- register-old.php
+++ register-old.php
@@ -1,6 +1,6 @@
 <?php
 // Include config file
-require_once "config.php";
+require_once "dbconfig.php";
  
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = "";
--- register.php
+++ register.php
@@ -1,13 +1,7 @@
 <?php
 // Include config file
-require_once "functions.php";
+require_once "dbconfig.php";
 
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 // Now we check if the data was submitted, isset() function will check if the data exists.
 if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
 	// Could not get the data that should have been sent.
