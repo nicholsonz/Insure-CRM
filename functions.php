b059fbbb18674381e8439d569942a439c9c0fd4e
Zachary Nicholson <ztnicholson@gmail.com>
2024-04-03 21:01:07 -0500

    modified:   functions.php 	modified:   home.php

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-    require_once('./functions.php');
+    include('dbconfig.php');
 
 // We need to use sessions, so you should always start sessions using the below code.
 if (session_status() === PHP_SESSION_NONE) {
