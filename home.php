106b02eea064fc33ebe9a3ec331ee6670b909ec2
Zach Nicholson <ztnicholson@gmail.com>
2024-04-16 16:48:33 -0500

    modified:   home.php

--- home.php
+++ home.php
@@ -12,99 +12,106 @@
     <div class="">
      <h2>Tasks</h2>
      <table class="w3-table">        
-	<tr>
-	 <th>#</th>
-	 <th>Task Name</th>
-	 <th>Priority</th>
-	 <th>Deadline</th>
-	</tr>
-	
-	<?php 
+		<tr>
+		<th>#</th>
+		<th>Task Name</th>
+		<th>Priority</th>
+		<th>Deadline</th>
+		</tr>
 		
-	//Select Database
-	$db_select = mysqli_select_db($con, DB_NAME) or die();
+		<?php 
 			
-	//Create SQL Query to Get DAta from Databse
-	$sql = "SELECT * FROM tbl_tasks ORDER BY deadline";
-		
-	//Execute Query
-	$res = mysqli_query($con, $sql);
+		//Select Database
+		$db_select = mysqli_select_db($con, DB_NAME) or die();
+				
+		//Create SQL Query to Get DAta from Databse
+		$sql = "SELECT * FROM tbl_tasks ORDER BY deadline";
 			
-	//CHeck whether the query execueted o rnot
-		if($res==true)
-		{
-		//DIsplay the Tasks from DAtabase
-		//Dount the Tasks on Database first
-		$count_rows = mysqli_num_rows($res);
+		//Execute Query
+		$res = mysqli_query($con, $sql);
 				
-		//Create Serial Number Variable
-		$sn=1;
-				
-		//Check whether there is task on database or not
-		if($count_rows>0)
-		{
-		//Data is in Database
-			while($row=mysqli_fetch_assoc($res))
+		//CHeck whether the query execueted o rnot
+			if($res==true)
 			{
-		 	 $task_id = $row['task_id'];
-			 $task_name = $row['task_name'];
-			 $priority = $row['priority'];
-			 $deadline = $row['deadline'];
-	?>
-						
-	<tr>
-	 <td><?php echo $sn++; ?>. </td>
-	 <td><?php echo $task_name; ?></td>
-	 <td><?php echo $priority; ?></td>
-	 <td><?php echo $deadline; ?></td>
-	</tr>
-						
-	<?php
+			//DIsplay the Tasks from DAtabase
+			//Dount the Tasks on Database first
+			$count_rows = mysqli_num_rows($res);
+					
+			//Create Serial Number Variable
+			$sn=1;
+					
+			//Check whether there is task on database or not
+			if($count_rows>0)
+			{
+			//Data is in Database
+				while($row=mysqli_fetch_assoc($res))
+				{
+				$task_id = $row['task_id'];
+				$task_name = $row['task_name'];
+				$priority = $row['priority'];
+				$deadline = $row['deadline'];
+		?>
+							
+		<tr>
+		<td><?php echo $sn++; ?>. </td>
+		<td><?php echo $task_name; ?></td>
+		<td><?php echo $priority; ?></td>
+		<td><?php echo $deadline; ?></td>
+		</tr>
+							
+		<?php
+				}
 			}
-		}
-		else
-		{
-		//No data in Database
-	?>					
-	<tr>
-	 <td colspan="5">No tasks ...</td>
-	</tr>					
-	<?php
+			else
+			{
+			//No data in Database
+		?>					
+		<tr>
+		<td colspan="5">No tasks ...</td>
+		</tr>					
+		<?php
+				}
 			}
-		}
-	
-	?>	
-       </table>
-      </div>
-      <div class="">
-	<h2><?php echo date('F'); ?> Activity</h2>
-	 <div>
+		
+		?>	
+     </table>
+	</div>
+	<div class="">
+		<h2><?php echo date('F'); ?> Activity</h2>
 		<?php
 			$pdo = pdo_connect_mysql();
 			$newleads = $pdo->query('SELECT COUNT(*) FROM leads WHERE MONTH(created) = MONTH(now())')->fetchColumn();
 			$convleads = $pdo->query('SELECT COUNT(*) FROM clients WHERE MONTH(created) = MONTH(now())')->fetchColumn();
+			$convperc = $convleads / $newleads;
 
 		?>
-	  <h4>Leads - <?= $newleads?></h4>
-	 </div>
-	 <div>
-	  <h4>Clients - <?= $convleads?></h4>
-	 </div> 
-      </div>
-      <div class="">
-	<h2><?php echo date('Y'); ?> Activity</h2>
-       <div>
+	<div>
+		<h4>Leads - <?= $newleads?></h4>
+	</div>
+	<div>
+		<h4>Clients - <?= $convleads?></h4>
+	</div>
+	<div>
+		<h4><?php echo date('F'); ?> Conversion = <?= $convperc * 100; ?> %</h4>
+	</div> 
+    </div>
+    <div class="">
+		<h2><?php echo date('Y'); ?> Activity</h2>
 		<?php
 			$pdo = pdo_connect_mysql();
 			$newleads = $pdo->query('SELECT COUNT(*) FROM leads WHERE YEAR(created) = YEAR(now())')->fetchColumn();
 			$convleads = $pdo->query('SELECT COUNT(*) FROM clients WHERE YEAR(created) = YEAR(now())')->fetchColumn();
-
+			$convperc = $convleads / $newleads;
 		?>
+       <div>
 	 <h4>Leads - <?= $newleads?></h4>
 	</div>
         <div>
 	 <h4>Clients - <?= $convleads?></h4>
         </div>
+	<div>
+		<h4><?php echo date('Y'); ?> Conversion = <?= $convperc * 100; ?> %</h4>
+	</div> 
        </div>
       </div>
       <div class="w3-col s12 m12 l7 w3-padding w3-margin">
