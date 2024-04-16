747388e642ead64c4aacff158ec06f4459517b11
Zach Nicholson <ztnicholson@gmail.com>
2024-04-16 17:06:40 -0500

    modified:   home.php

--- home.php
+++ home.php
@@ -9,74 +9,7 @@
 <div class="content w3-mobile">
  <h1><?php echo date('M d, Y'); ?></h1>
    <div class="w3-col s12 m4 l4 w3-margin">
-    <div class="">
-     <h2>Tasks</h2>
-     <table class="w3-table">        
-		<tr>
-		<th>#</th>
-		<th>Task Name</th>
-		<th>Priority</th>
-		<th>Deadline</th>
-		</tr>
-		
-		<?php 
-			
-		//Select Database
-		$db_select = mysqli_select_db($con, DB_NAME) or die();
-				
-		//Create SQL Query to Get DAta from Databse
-		$sql = "SELECT * FROM tbl_tasks ORDER BY deadline";
-			
-		//Execute Query
-		$res = mysqli_query($con, $sql);
-				
-		//CHeck whether the query execueted o rnot
-			if($res==true)
-			{
-			//DIsplay the Tasks from DAtabase
-			//Dount the Tasks on Database first
-			$count_rows = mysqli_num_rows($res);
-					
-			//Create Serial Number Variable
-			$sn=1;
-					
-			//Check whether there is task on database or not
-			if($count_rows>0)
-			{
-			//Data is in Database
-				while($row=mysqli_fetch_assoc($res))
-				{
-				$task_id = $row['task_id'];
-				$task_name = $row['task_name'];
-				$priority = $row['priority'];
-				$deadline = $row['deadline'];
-		?>
-							
-		<tr>
-		<td><?php echo $sn++; ?>. </td>
-		<td><?php echo $task_name; ?></td>
-		<td><?php echo $priority; ?></td>
-		<td><?php echo $deadline; ?></td>
-		</tr>
-							
-		<?php
-				}
-			}
-			else
-			{
-			//No data in Database
-		?>					
-		<tr>
-		<td colspan="5">No tasks ...</td>
-		</tr>					
-		<?php
-				}
-			}
-		
-		?>	
-     </table>
-	</div>
-	<div class="">
+	<div class="w3-padding">
 		<h2><?php echo date('F'); ?> Activity</h2>
 		<?php
 			$pdo = pdo_connect_mysql();
@@ -112,20 +45,94 @@
 	<div>
 		<h4><?php echo date('Y'); ?> Conversion = <?= $convperc * 100; ?> %</h4>
 	</div> 
-       </div>
-      </div>
-      <div class="w3-col s12 m12 l7 w3-padding w3-margin">
-  	<h2><?php echo date('Y'); ?> Activity</h2>
-  		<?php
-			$pdo = pdo_connect_mysql();
-			$mnthleads = $pdo->query('SELECT DATE_FORMAT(created, "%b") as monthname, COUNT(*) as leadnum FROM leads WHERE YEAR(created) = YEAR(now()) GROUP BY monthname')->fetchAll(PDO::FETCH_OBJ);
-			$mnthclients = $pdo->query('SELECT DATE_FORMAT(created, "%b") as monthname, COUNT(*) as clientnum FROM clients WHERE YEAR(created) = YEAR(now()) GROUP BY monthname')->fetchAll(PDO::FETCH_OBJ);
+   </div>
+    <div class="">
+     <h2>Tasks</h2>
+     <table class="w3-table">        
+		<tr>
+		<th>#</th>
+		<th>Task Name</th>
+		<th>Priority</th>
+		<th>Task List</th>
+		<th>Deadline</th>
+		</tr>
+		
+		<?php 
+			
+		//Select Database
+		$db_select = mysqli_select_db($con, DB_NAME) or die();
+				
+		//Create SQL Query to Get DAta from Databse
+		$sql = "SELECT *, tbll.list_name 
+				FROM tbl_tasks AS tblt
+				LEFT JOIN tbl_lists AS tbll ON tblt.list_id = tbll.list_id
+				ORDER BY deadline";
+			
+		//Execute Query
+		$res = mysqli_query($con, $sql);
+				
+		//CHeck whether the query execueted o rnot
+			if($res==true)
+			{
+			//DIsplay the Tasks from DAtabase
+			//Dount the Tasks on Database first
+			$count_rows = mysqli_num_rows($res);
+					
+			//Create Serial Number Variable
+			$sn=1;
+					
+			//Check whether there is task on database or not
+			if($count_rows>0)
+			{
+			//Data is in Database
+				while($row=mysqli_fetch_assoc($res))
+				{
+				$task_id = $row['task_id'];
+				$task_name = $row['task_name'];
+				$priority = $row['priority'];
+				$list_name = $row['list_name'];
+				$deadline = $row['deadline'];
 		?>
+							
+		<tr>
+		<td><?php echo $sn++; ?>. </td>
+		<td><?php echo $task_name; ?></td>
+		<td><?php echo $priority; ?></td>
+		<td><?php echo $list_name; ?></td>
+		<td><?php echo $deadline; ?></td>
+		</tr>
+							
+		<?php
+				}
+			}
+			else
+			{
+			//No data in Database
+		?>					
+		<tr>
+		<td colspan="5">No tasks ...</td>
+		</tr>					
+		<?php
+				}
+			}
+		
+		?>	
+     </table>
+	</div>
+ </div>
+ <div class="w3-col s12 m12 l7 w3-margin">
+	<div class="w3-padding">
+	<h2><?php echo date('Y'); ?> Activity</h2>
+	<?php
+		$pdo = pdo_connect_mysql();
+		$mnthleads = $pdo->query('SELECT DATE_FORMAT(created, "%b") as monthname, COUNT(*) as leadnum FROM leads WHERE YEAR(created) = YEAR(now()) GROUP BY monthname')->fetchAll(PDO::FETCH_OBJ);
+		$mnthclients = $pdo->query('SELECT DATE_FORMAT(created, "%b") as monthname, COUNT(*) as clientnum FROM clients WHERE YEAR(created) = YEAR(now()) GROUP BY monthname')->fetchAll(PDO::FETCH_OBJ);
+	?>
 
 	<canvas id="activityChart" class="chart-style"></canvas>
-
-       </div>
-     </div>
+ </div>
+</div>
+</div>
 <script type="text/javascript">
 	const mnthleads = <?php echo json_encode($mnthleads) ?>;
 	const mnthclients = <?php echo json_encode($mnthclients) ?>;
