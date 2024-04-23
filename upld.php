135b44d372295390a0131304dbbe0494736ca4d1
Zach Nicholson <ztnicholson@gmail.com>
2024-04-23 11:02:06 -0500

    modified:   upld.php

--- upld.php
+++ upld.php
@@ -1,4 +1,14 @@
 <?php
+// We need to use sessions, so you should always start sessions using the below code.
+if (session_status() === PHP_SESSION_NONE) {
+  session_start();
+}
+
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+header("location: index.php");
+exit;
+}
 
 // prevent execution of the file directly
 if(!isset($_POST['submit'])){
