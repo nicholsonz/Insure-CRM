b902a1305077545cf51fe9e2b8e76ef024447994
Zach Nicholson <ztnicholson@gmail.com>
2024-04-23 10:44:37 -0500

    modified:   upld.php

--- upld.php
+++ upld.php
@@ -10,11 +10,15 @@ $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = filesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "File is a pdf - " . $check["mime"] . ".";
+    echo "File has been uploaded successfully - " . $check["mime"] . ".";
     $uploadOk = 1;
+    sleep(7);
+    header('Location: clients.php');
   } else {
     echo "File is not an acceptable format.";
     $uploadOk = 0;
+    sleep(7);
+    header('Location: clients.php');
   }
 }
 
@@ -22,29 +26,43 @@ if(isset($_POST["submit"])) {
 if (file_exists($target_file)) {
   echo "Sorry, file already exists.";
   $uploadOk = 0;
+  sleep(7);
+  header('Location: clients.php');
+
 }
 
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 1000000) {
   echo "Sorry, your file is too large.";
   $uploadOk = 0;
+  sleep(7);
+  header('Location: clients.php');
+
 }
 
 // Allow certain file formats
 if($fileType != "pdf" && $fileType != "odt" && $fileType != "docx" && $fileType != "doc" ) {
   echo "Sorry, only PDF, ODT, DOCX, and DOC files are allowed.";
   $uploadOk = 0;
+  sleep(7);
+  header('Location: clients.php');
 }
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
+  sleep(7);
+  header('Location: clients.php');
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    sleep(7);
+    header('Location: clients.php');
   } else {
     echo "Sorry, there was an error uploading your file.";
+    sleep(7);
+    header('Location: clients.php');
   }
 }
 ?>
