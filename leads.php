a4dd234a978ef6214f57e67d4a456f33a7a1581b
Zach Nicholson <ztnicholson@gmail.com>
2024-05-07 13:07:43 -0500

    modified:   leads.php

--- leads.php
+++ leads.php
@@ -9,7 +9,7 @@ $page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] :
 $records_per_page = 12;
 
 // Prepare the SQL statement and get records from our leads table, LIMIT will determine the page
-$stmt = $pdo->prepare('SELECT * FROM leads ORDER BY phone LIMIT :current_page, :record_per_page');
+$stmt = $pdo->prepare('SELECT * FROM leads ORDER BY created DESC LIMIT :current_page, :record_per_page');
 $stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
 $stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
 $stmt->execute();
