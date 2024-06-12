acafab7316a30abd9976c5fbec2216437dcb9d55
Zach Nicholson <ztnicholson@gmail.com>
2024-06-11 23:36:05 -0500

    add favicon to login page

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Login</title>
+		<title>Login - Simple-CRM</title>
 		<link rel="stylesheet" href="../fontawesome/css/all.css">
 		<link href="./css/style.css" rel="stylesheet" type="text/css">
+		<link rel="icon" href="./favicon.ico">
 	</head>
 	<body class="logincolor">
 		<div class="login">
-			<h1>Login</h1>
+			<h1>Simple-CRM</h1>
 			<form action="./action/authenticate.php" method="post">
 				<label for="username">
 					<i class="fas fa-user"></i>
