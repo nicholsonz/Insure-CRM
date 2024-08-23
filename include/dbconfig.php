db81f6c478f41a8d11eb4858e1d6d64d90df60f3
Zach Nicholson <ztnicholson@gmail.com>
2024-08-23 13:10:25 -0500

    include dbconfig.php

---
+++ include/dbconfig.php
@@ -0,0 +1,26 @@
+<?php
+// PDO db creds
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = '';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'clientdb';
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	exit('Failed to connect to database!');
+    }
+}
+
+// mysqli db creds
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'clientdb');
+
+$con = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+if($con === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
