40f8ef59066cdbe5621b8ce557dfa1f7b9c707eb
Zach Nicholson <ztnicholson@gmail.com>
2024-12-19 09:53:15 -0600

    update session id security

--- require/header.php
+++ require/header.php
@@ -5,9 +5,9 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
   }
-
+$key = $_SESSION["loggedin"];
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== $key){
 	header("Location: index.php");
   exit();
    }
